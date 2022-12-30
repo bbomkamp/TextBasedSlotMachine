353df995dbb7f8e113857c8c44320c882370997c
bbomkamp <bradbomkamp@gmail.com>
2022-12-30 00:13:20 -0600

    Methods for getting Users Bets

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
+#CONSTANTS(Final)
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 # Retrieves User Deposit.
 def deposit():
@@ -6,8 +9,10 @@ def deposit():
     #Loops til a number is returned.
     while True:
         amount = input("How much would you like to deposit to your total? $")
+
         #Check if input is postive number.
         if amount.isdigit():
+
             #Convert the inputted String to an Int.
             amount = int(amount)
             if amount > 0:
@@ -19,7 +24,52 @@ def deposit():
 
     return amount
 
+def get_number_of_lines():
+
+    #Loops til a number is returned.
+    while True:
+        lines = input("How many lines would you like to bet on (1-" + str(MAX_LINES) + ")? ")
+
+        #Check if input is postive number.
+        if lines.isdigit():
+
+            #Convert the inputted String to an Int.
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter and number between 1 - " + str(MAX_LINES))
+        else:
+            print("Please enter a number.")
+
+    return lines    
+
+def get_bet():
+
+    #Loops til a number is returned.
+    while True:
+        amount = input("How much would you like to bet on each line? $")
+
+        #Check if input is postive number.
+        if amount.isdigit():
+
+            #Convert the inputted String to an Int.
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+    
+
 def main():
-    balance = deposit()    
+    balance = deposit()
+    lines = get_number_of_lines()
+    bet = get_bet()
+
+    print(balance, lines)    
 
 main()
